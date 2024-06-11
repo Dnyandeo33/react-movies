f80af0b19ba41affae95ef5c39357fc876746b92
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-06-11 08:55:56 +0200

    update

--- src/apis/getMovies.js
+++ src/apis/getMovies.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const getMovies = async (value) => {
+    const search = value || 'indian'
     try {
-        const res = await axios.get(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${value}&page=1`)
+        const res = await axios.get(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${search}&page=1`)
         if (res.status !== 200) {
             throw new Error(res.statusText)
         }
